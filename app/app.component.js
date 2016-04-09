d43dc21cbba58aaed29e9397c057575dafd7d169
Nate Morse <morse.nate@gmail.com>
2016-04-08 21:32:10 -0400

    compile to js

--- app/app.component.js
+++ app/app.component.js
@@ -1,5 +1,6 @@
-System.register(['angular2/core', 'angular2/src/facade/lang'], function(exports_1) {
+System.register(['angular2/core', 'angular2/src/facade/lang'], function(exports_1, context_1) {
     "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -204,7 +205,7 @@ System.register(['angular2/core', 'angular2/src/facade/lang'], function(exports_
                 Forth.prototype.displayHeap = function () {
                 };
                 return Forth;
-            })();
+            }());
             AppComponent = (function () {
                 function AppComponent() {
                     this.forth = new Forth();
@@ -249,7 +250,7 @@ System.register(['angular2/core', 'angular2/src/facade/lang'], function(exports_
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
-            })();
+            }());
             exports_1("AppComponent", AppComponent);
         }
     }
